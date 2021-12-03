d0610f36ce2b95f2f585e861b8bfd1d1f3408a31
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-03 14:21:01 +0100

    Stampa costo

--- js/script.js
+++ js/script.js
@@ -2,13 +2,25 @@
 //creo 2 variabili per km ed età
 //creo una costante per il costo di un km
 //ed una variabile per il costo del biglietto
+let km = prompt('Inserisci i km da percorrere');
+let age = prompt('Inserisci la tua età');
+const kmPrice = 0.21;
+let ticketPrice;
 
 //calcolo il costo del biglietto
+ticketPrice= km * kmPrice;
+//console.log(ticketPrice);
 
 //se l'età è <18 calcolo il costo pieno
 //scontato del 20% arrotondato a 2 decimali
+if(age < 18)
+    ticketPrice= ticketPrice*0.8;
 
 //se l'età è >=65 clacolo il costo pieno
 //scontato del 40% arrotondato a 2 decimali
+if(age > 65)
+    ticketPrice= ticketPrice*0.6;
 
 //stampo il costo del biglietto
+let htmlElement = document.getElementById('stamp');
+htmlElement.innerHTML= `Il costo del tuo biglietto sarà: ${ticketPrice}`;
