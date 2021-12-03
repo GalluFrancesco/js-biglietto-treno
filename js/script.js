d4436c53db7b6ff3359788f78457f8b457ad77aa
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-03 14:03:04 +0100

    Schema procedimento

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//chiedo all'utente i dati e
+//creo 2 variabili per km ed età
+//creo una costante per il costo di un km
+//ed una variabile per il costo del biglietto
+
+//calcolo il costo del biglietto
+
+//se l'età è <18 calcolo il costo pieno
+//scontato del 20% arrotondato a 2 decimali
+
+//se l'età è >=65 clacolo il costo pieno
+//scontato del 40% arrotondato a 2 decimali
+
+//stampo il costo del biglietto
